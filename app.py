bff5f47f682966c4c58c838907ca1ce730a61128
Mahitha Guduru <mahithaofficial007@gmail.com>
2025-03-19 16:16:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,8 @@ if model_choice != "Select Model":
             input_arr = input_arr.reshape(1, 128, 128, 3)
         else:
             input_arr = np.expand_dims(input_arr, axis=0)  # (1, 224, 224, 3)
+            feature_extractor = tf.keras.Model(model.input, model.layers[-2].output)  # Get feature layer output
+            input_arr = feature_extractor.predict(input_arr) 
            
 
 
